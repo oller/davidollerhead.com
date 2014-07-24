239a6d77cc542fe0d620279cf3c85a7b39218100
David Ollerhead <david@davidollerhead.com>
2014-07-24 15:23:02 +0100

    on dom ready...

--- _assetsToCompile/js/work.js
+++ _assetsToCompile/js/work.js
@@ -1,27 +1,29 @@
 'use strict';
 
-// Once 'inview', fade in portfolio work
-$('.tile-fade').one('inview', function() {
-    $('.faded').each(function(i) {
+$(function() {
+    // Once 'inview', fade in portfolio work
+    $('.tile-fade').one('inview', function () {
+      $('.faded').each(function (i) {
         var $this = $(this);
-        setTimeout(function() {
-            $this.removeClass('faded');
+        setTimeout(function () {
+          $this.removeClass('faded');
         }, i * 80);
+      });
     });
-});
 
-// Filter Porfolio Work Grid by 'area', specified by data-filter attribute
-$('.grid-filter a').click(function(e) {
-    var $this = $(this);
-    $('.grid-filter a').removeClass('filter');
-    $this.addClass('filter');
-    var tag = $this.data('filter');
-    if (tag == "all") {
+    // Filter Porfolio Work Grid by 'area', specified by data-filter attribute
+    $('.grid-filter a').click(function (e) {
+      var $this = $(this);
+      $('.grid-filter a').removeClass('filter');
+      $this.addClass('filter');
+      var tag = $this.data('filter');
+      if (tag == "all") {
         $('.grid-node').removeClass('masked');
-    } else {
+      } else {
         var $matches = $('.grid-node.' + tag);
         $('.grid-node').not($matches).addClass('masked');
         $matches.removeClass('masked');
-    }
-    e.preventDefault();
+      }
+      e.preventDefault();
+    });
 });
