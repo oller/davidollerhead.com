8a12cfc05614ebb8a5b7599a31e73059002d59d0
oller <david.ollerhead@gmail.com>
2014-08-17 20:28:26 +0100

    Contact Form now submits

--- app/contact/index.markdown
+++ app/contact/index.markdown
@@ -10,17 +10,15 @@ navigation:
 
 You can get me at [@DavidOllerhead][twitter], or use the form below.
 
-<form action="" class="">
-    <fieldset class="">
+<form id="contact-form">
+    <fieldset>
         <input type="text" class="form__field" placeholder="Name" name="name" required="required" autofocus="autofocus" />
         <input type="email" class="form__field" placeholder="Email" name="email" required="required" />
         <textarea class="form__field" name="message" placeholder="Message" rows="8" required="required"></textarea>
     </fieldset>
-
-    <button type="submit" class="form__submit">Send Message</button>
+    <button type="submit" class="form__submit" id="submit">Send Message</button>
 </form>
 
 My <abbr title="Curriculum Vitae">CV</abbr> is available on request.
-
 
 [twitter]: http://www.twitter.com/davidollerhead
--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,92 @@
-$(function() {
-    // Filter Porfolio Work Grid by 'area', specified by data-filter attribute
+'use strict';
 
-    var activeClass = 'active';
+(function() {
 
-    $('.portfolio-grid__filter a').click(function(e) {
-        var $this = $(this);
-        $('.portfolio-grid__filter a').removeClass(activeClass);
-        $this.addClass(activeClass);
-        var tag = $this.data('filter');
-        if (tag === 'all') {
-            $('.portfolio-grid__work').removeClass('masked');
-        } else {
-            var $matches = $('.portfolio-grid__work.' + 'tag-' + tag);
-            $('.portfolio-grid__work').not($matches).addClass('masked');
-            $matches.removeClass('masked');
+    var davidollerhead = {
+
+        init: function() {
+            this.gridFilter();
+            this.contactForm();
+        },
+
+        gridFilter: function() {
+            // Filter Porfolio Work Grid by 'area', specified by data-filter attribute
+            var activeClass = 'active';
+
+            $('.portfolio-grid__filter a').click(function(e) {
+                var $this = $(this);
+                $('.portfolio-grid__filter a').removeClass(activeClass);
+                $this.addClass(activeClass);
+                var tag = $this.data('filter');
+                if (tag === 'all') {
+                    $('.portfolio-grid__work').removeClass('masked');
+                } else {
+                    var $matches = $('.portfolio-grid__work.' + 'tag-' + tag);
+                    $('.portfolio-grid__work').not($matches).addClass('masked');
+                    $matches.removeClass('masked');
+                }
+                e.preventDefault();
+            });
+        },
+
+        contactForm: function() {
+            // Ajax submit Contact Form
+            var request;
+            var simpleFormToken = '207a5cba6b522a6c696626e9d2cf6c1d';
+
+            // Bind to the submit of message form
+            $('#contact-form').submit(function(event) {
+                // Abort any pending request
+                if (request) {
+                    request.abort();
+                }
+                // Setup and cache some local vars
+                var form = $(this);
+                var inputs = form.find('input, select, button, textarea');
+                var serializedData = form.serialize();
+
+                console.log(form);
+                console.log(inputs);
+                console.log(serializedData);
+
+                // Disable inputs during .post
+                inputs.prop('disabled', true);
+
+                // Fire off the request to /form.php
+                request = $.ajax({
+                    dataType: 'jsonp',
+                    url: 'http://getsimpleform.com/messages/ajax?form_api_token=' + simpleFormToken,
+                    type: 'post',
+                    data: serializedData
+                });
+
+                // Callback on success
+                request.done(function(response, textStatus, jqXHR) {
+                    // positive feedback to user
+                    $('#submit').prop('disabled', true).addClass('form__submit--success').text('\u2713 Message Sent!');
+                });
+
+                // Callback on failure
+                request.fail(function(jqXHR, textStatus, errorThrown) {
+                    // Log error to the console
+                    console.error('The following error occured: ' + textStatus, errorThrown);
+                });
+
+                // Callback regardless
+                request.always(function() {
+                    // Reenable inputs
+                    inputs.not('button').prop('disabled', false);
+                });
+
+                // Prevent posting of form
+                event.preventDefault();
+            });
         }
-        e.preventDefault();
+    };
+
+    // R-r-r-ready
+    $(function() {
+        davidollerhead.init();
     });
-});
+
+}());
