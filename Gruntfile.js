23b0a2c93f887172e3d5bb154263ea57464c0b02
oller <david.ollerhead@gmail.com>
2014-08-22 14:49:35 +0100

    Suppressing minified portfolio JS from jshinting

--- Gruntfile.js
+++ Gruntfile.js
@@ -297,6 +297,7 @@ module.exports = function (grunt) {
       all: [
         'Gruntfile.js',
         '<%= yeoman.app %>/js/**/*.js',
+        '!<%= yeoman.app %>/js/work/**/*.js',
         'test/spec/**/*.js'
       ]
     },
