a49db419f6657a42180cfcb42b84b6a56bab8d37
oller <david.ollerhead@gmail.com>
2014-11-14 11:30:35 +0000

    Disabling imagemin until required

--- Gruntfile.js
+++ Gruntfile.js
@@ -409,7 +409,7 @@ module.exports = function(grunt) {
         'autoprefixer:dist',
         'cssmin',
         'uglify',
-        'imagemin',
+        // 'imagemin',
         'svgmin',
         'stripmq',
         'filerev',
